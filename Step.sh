581d26a591add3069bb136ce01f119dde986d158
adamkhobba <adamekhobba6@gmail.com>
2024-02-03 10:52:40 +0100

    create a file that help me to accelrate my step of linux in 1337

---
+++ Step.sh
@@ -0,0 +1,30 @@
+# Program
+sudo apt install git
+sudo apt install curl
+sudo apt install vim
+sudo apt install 
+
+# AstroNvim
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim
+
+# Zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# 10k power level
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# Discord    
+sudo apt install snpad
+snpad install discord
+
+# Obsidian
+# https://obsidian.md/download
+snap install obsidian*.snap --dangerous --classic
+
+# 
