7c7dfe8c9b58b72bd676081e853aa4027e547bfe
adamkhobba <129398945+adamkhobba@users.noreply.github.com>
2024-02-14 10:03:53 +0100

    edit .zshrc

--- Step.sh
+++ Step.sh
@@ -17,7 +17,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # 10k power level
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-ZSH_THEME="powerlevel10k/powerlevel10k"
+echo ZSH_THEME="powerlevel10k/powerlevel10k" > ~/.zshrc
 
 # Discord    
 sudo apt install snpad
