7afae93dd87f7fb76a82c0930058d00bdc1e0dc1
adamekhobba <adamekhobba6@gmail.com>
2024-02-20 11:40:12 +0100

    add Neovim + add obsidian

--- Step.sh
+++ Step.sh
@@ -1,4 +1,4 @@
-# Program
+#Program
 sudo apt install git
 sudo apt install curl
 sudo apt install vim
@@ -27,4 +27,20 @@ snpad install discord
 # https://obsidian.md/download
 snap install obsidian*.snap --dangerous --classic
 
-# 
+# Neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+         # Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+nvim
+
+# Neurd font
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf 
+mkdir .local/share/fonts
+mv HackNerdFont-Regular.ttf .local/share/fonts
+
